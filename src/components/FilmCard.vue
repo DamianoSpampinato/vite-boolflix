c28505cb79c8e0a62f7f7fb6a60d66e2993d159f
Damiano <ghgg585@gmail.com>
2024-04-17 20:07:03 +0200

    feat: add img lang to film card

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -5,6 +5,14 @@ export default{
     props:{
         filmInfo: Object
     },
+    data(){
+        flagArray: [
+            {
+                lang: 'it',
+               
+            }
+        ]
+    }
     
 
 }
@@ -13,8 +21,12 @@ export default{
 <div class="card">
     <h5>{{ filmInfo.title }}</h5>
     <small>{{ filmInfo.original_title }}</small>
-    <div>{{ filmInfo.original_language }}</div>
-    
+    <div class="img-container">
+        <img :src="`/src/assets/img/${filmInfo.original_language}.png`" :alt="filmInfo.original_language">
+    </div>
+
+
+
     <!-- VOTO MEDIO ARROTONDATO -->
     <div>{{ Math.ceil(this.filmInfo.vote_average) }}</div>
 </div>
@@ -23,5 +35,13 @@ export default{
 <style scoped lang="scss">
 .card{
     border: 1px solid red;
+    .img-container{
+        width: 70px;
+        height: 70px;
+        img{
+            width: 100%;
+            height: 100%;
+        }
+    }
 }
 </style>
